8dde36282bf45961eb01bb0544e5aa1ee01a59a7
Styvens Wentz <styvens.wentz@gmail.com>
2025-02-06 09:23:35 +0100

    Ajout de swup

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
