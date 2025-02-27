b8a32b861cfb39feb2d5e3578ab90ffa9e12fc74
Styvens Wentz <styvens.wentz@gmail.com>
2025-02-27 12:16:08 +0100

    séparation des carte du portfolio et trie du plus récent au plus ancien

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -19,7 +19,7 @@ final class ActivityController extends AbstractController
     public function index(ActivityRepository $activityRepository): Response
     {
         return $this->render('admin/activity/index.html.twig', [
-            'activities' => $activityRepository->findAll(),
+            'activities' => $activityRepository->findBy([], ['end' => 'DESC']),
         ]);
     }
 
